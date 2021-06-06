38f0ac0a923cc1b40121c60a681792e1ec84706a
gonzoden <deniz.nazarova.99@gmail.com>
2021-06-07 00:13:04 +0600

    correct task

--- 1012.py
+++ 1012.py
@@ -1,12 +1,23 @@
 import math
 
-a = float(input())
-b = float(input())
-c = float(input())
+a, b, c = list(map(float, input().split(" ")))
+#map function completes function "float" with input.split(" ") as an arg
+#list is one of 4 built-in data types for storing data, changeable
 
-print("TRIANGULO: ", round((a*(1/2)*c), 3))
-print("CIRCULO: ", round(math.pi*(c**2), 3))
-print("TRAPEZIO: ", round((1/2)*(a+b)*c, 3))
-print("QUADRADO: ", round(b**2, 3))
-print("RETANGULO: ", round(a*b, 3))
+pi = round(math.pi, 5) 
+#not that sure whether reserving memory for the new var
+#is a good decision, yet it is definitely better than magic
 
+triangle = a*(1/2)*c
+cicrcle = pi*(c**2)
+trapezium = (1/2)*(a+b)*c
+square = b**2
+rectangle = a*b
+#round() cannot be used when there is exact precision of 3 digits
+#after zero no matter what, so you have to do this mumbo jumbo
+
+print("TRIANGULO: %.3f" % triangle)
+print("CIRCULO: %.3f" % cicrcle)
+print("TRAPEZIO: %.3f" % trapezium)
+print("QUADRADO: %.3f" % square)
+print("RETANGULO: %.3f" % rectangle)
