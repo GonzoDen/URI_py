dcf20277be67533967fb4b17b8139c25290ba82f
gonzoden <deniz.nazarova.99@gmail.com>
2021-06-08 15:18:40 +0600

    daily uri

---
+++ 1013.py
@@ -0,0 +1,14 @@
+line = input().split(" ")
+
+a, b, c = line
+
+def maior(a, b):
+	maior = (int(a) + int(b) + abs(int(a)-int(b)))/2
+	return maior
+
+result = maior(a, b)
+result_fin = maior(result, c)
+
+print("%d eh o maior" % result_fin)
+
+
