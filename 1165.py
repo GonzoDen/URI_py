d3745e70f04a2d104ae423939df8a8d28f4d4eab
gonzoden <deniz.nazarova.99@gmail.com>
2021-07-03 00:25:07 +0600

    daily uri

--- 1165.py
+++ 1165.py
@@ -1,15 +1,22 @@
+#TODO: correct
+
 import math
 
-isPrime = False
+isPrime = True
 
 n = int(input())
 
-for i in range(1, n+1):
+for i in range(n):
 	num = int(input())
 
-	for j in range(2, int(math.sqrt(float(num)))):
-		if num/j == 0:
-			break
+	for j in range(1, num+1):
+
+		if num % j == 0 and j != 1 and j != num:
+			isPrime = False
 		else:
 			isPrime == True
-	print(isPrime)
+
+	if isPrime == True:
+		print("%d eh primo" % num)
+	else:
+		print("%d nao eh primo" % num)
